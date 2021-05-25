af6aaffce395d3462a263323ff88bb91f3db26e7
geraldoam <geraldoahnert@gmail.com>
2021-05-25 20:09:41 -0300

    Add: First component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
+import Navbar from "./components/Navbar.js";
 
 function App() {
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>{!data ? "Loading..." : data}</p>
-            </header>
-        </div>
-    );
+    return <Navbar />;
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <div className="bg-pink-500">
+            <a href="#home">Home</a>
+            <a href="#home">Register</a>
+        </div>
+    );
+}
+
+export default Navbar;
