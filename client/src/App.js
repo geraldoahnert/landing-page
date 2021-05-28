cf323b7627d2a70295137e2a1704422a0b5a0261
geraldoam <geraldoahnert@gmail.com>
2021-05-28 19:43:11 -0300

    Add: home page component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./components/Navbar.js";
+import FirstPage from "./components/FirstPage.js";
 
 function App() {
     return (
         <Router>
             <Navbar />
+            <FirstPage />
         </Router>
     );
 }
