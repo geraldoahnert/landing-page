990e25d9bb702b5a708e2ed76c75d318f6ccf461
geraldoam <geraldoahnert@gmail.com>
2021-06-01 20:28:01 -0300

    fix customization firstpage and landingpage

--- client/src/components/FirstPage.js
+++ client/src/components/FirstPage.js
@@ -2,8 +2,10 @@ function FirstPage() {
     return (
         <div className="mt-40">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                    <span className="block text-indigo-600">Landing Page</span>
+                <h2 className="text-3xl tracking-tight text-gray-900 sm:text-4xl">
+                    <span className="block text-indigo-600 font-extrabold">
+                        Landing Page
+                    </span>
                     <span className="block sm:text-2xl">
                         Register and get all news on your email.
                     </span>
