2799e23c03444ecb7f7aa0d4f141a557d5fc0e45
geraldoam <geraldoahnert@gmail.com>
2021-06-16 19:28:22 -0300

    feat: email controller

---
+++ server/src/controllers/EmailController.js
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import Email from "../models/Email";
+
+class EmailController {}
+
+export default new EmailController();
