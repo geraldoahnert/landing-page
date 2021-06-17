088ee4ac259191406869e439fc3609d541f73a70
geraldoam <geraldoahnert@gmail.com>
2021-06-17 20:28:32 -0300

    feat: change old entity emails to users

---
+++ server/src/models/User.ts
@@ -0,0 +1,12 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    email: string;
+}
+
+export default User;
