aacb4931760fa4599aec8bfbba62127118361178
geraldoam <geraldoahnert@gmail.com>
2021-06-17 20:27:48 -0300

    del: old files / turn to typescript

--- server/src/controllers/EmailController.js
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-
-import Email from "../models/Email";
-
-class EmailController {}
-
-export default new EmailController();
--- server/src/routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-import EmailController from "./controllers/EmailController.js";
-
-const router = Router();
-
-router.post("/register", EmailController);
