ebf29f6fe9192a0268ae19136c20fe20165080ab
geraldoam <geraldoahnert@gmail.com>
2021-06-28 20:09:13 -0300

    fix: handle method

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import UserController from "./controllers/UserController";
+import UserController from './controllers/UserController';
 
 const router = Router();
 
-router.post("/register", UserController.store);
+router.post('/register', UserController.handle);
 
 export default router;
