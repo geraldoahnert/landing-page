1e03ed7f61ea354e63e36356593488becf960c25
geraldoam <geraldoahnert@gmail.com>
2021-06-28 20:07:00 -0300

    fix: only controller task

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,24 +1,35 @@
-import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import User from "../models/User";
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
+import { UserService } from '../services/UserService';
 
 class EmailController {
-    async store(req: Request, res: Response) {
+    async handle(req: Request, res: Response) {
         const repository = getRepository(User);
 
         const { email } = req.body;
 
-        const userExists = await repository.findOne({ where: { email } });
+        const userService = new UserService();
 
-        if (userExists) {
-            return res.sendStatus(409);
-        }
-
-        const user = repository.create({ email });
-
-        await repository.save(user);
+        const user = await userService.execute({
+            email,
+        });
 
         return res.json(user);
+
+        // Service
+
+        // const userExists = await repository.findOne({ where: { email } });
+
+        // if (userExists) {
+        //     return res.sendStatus(409);
+        // }
+
+        // const user = repository.create({ email });
+
+        // await repository.save(user);
+
+        // return res.json(user);
     }
 }
 
