73238ac3b532aecffc7fbccf88f823acd971b4a4
geraldoam <geraldoahnert@gmail.com>
2021-06-28 18:49:47 -0300

    refactor: short way route

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Navbar from "./components/Navbar";
-import FirstPage from "./pages/FirstPage";
-import LandingPage from "./pages/LandingPage";
+import Navbar from './components/Navbar';
+import FirstPage from './pages/FirstPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
     return (
         <Router>
             <Navbar />
-
             <Switch>
-                <Route exact path="/">
-                    <FirstPage />
-                </Route>
-                <Route path="/register">
-                    <LandingPage />
-                </Route>
+                <Route exact path="/" component={FirstPage} />
+                <Route path="/register" component={LandingPage} />
             </Switch>
         </Router>
     );
